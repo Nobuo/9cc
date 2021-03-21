bbc5ed2f59804e9235351c0c3c437aa6dbe4dffb
Nobuo Yoshie <ynobuo72@gmail.com>
2021-03-22 08:32:41 +0900

    ステップ6：単項プラスと単項マイナス

--- 9cc.c
+++ 9cc.c
@@ -174,6 +174,7 @@ Node *new_num(int val) {
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
 
 Node *expr() {
   Node *node = mul();
@@ -188,19 +189,27 @@ Node *expr() {
   }
 }
 
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_binary(ND_MUL, node, primary());
+      node = new_binary(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_binary(ND_DIV, node, primary());
+      node = new_binary(ND_DIV, node, unary());
     else
       return node;
   }
 }
 
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_binary(ND_SUB, new_num(0), unary());
+  return primary();
+}
 
 Node *primary() {
   // 次のトークンが"("なら、"(" expr ")"のはず
