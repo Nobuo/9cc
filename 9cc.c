4d773d959de7369dacb25cb514a0b760b8b39b79
Nobuo Yoshie <ynobuo72@gmail.com>
2021-05-08 21:05:17 +0900

    ステップ8: ファイル分割とMakefileの変更

--- 9cc.c
+++ 9cc.c
@@ -1,26 +1,4 @@
-#include <ctype.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// トークンの種類
-typedef enum {
-  TK_RESERVED, // 記号
-  TK_NUM,      // 整数トークン
-  TK_EOF,      // 入力の終わりを表すトークン
-} TokenKind;
-
-// トークン型
-typedef struct Token Token;
-struct Token {
-  TokenKind kind; // トークンの型
-  Token *next;    // 次の入力トークン
-  int val;        // kindがTK_NUMの場合、その数値
-  char *str;      // トークン文字列
-  int len;        // トークンの長さ
-};
+#include "9cc.h"
 
 // 入力プログラム
 char *user_input;
@@ -148,29 +126,6 @@ Token *tokenize() {
 //
 // パーサー
 // 
-
-// 抽象構文木のノードの種類
-typedef enum {
-  ND_ADD, // +
-  ND_SUB, // -
-  ND_MUL, // *
-  ND_DIV, // /
-  ND_EQ,  // ==
-  ND_NE,  // !=
-  ND_LT,  // <
-  ND_LE,  // <=
-  ND_NUM, // 整数
-} NodeKind;
-
-
-// 抽象構文木のノードの型
-typedef struct Node Node;
-struct Node {
-  NodeKind kind; // ノードの型
-  Node *lhs;     // 左辺
-  Node *rhs;     // 右辺
-  int val;       // kindがND_NUMの場合のみ使う
-};
 
 Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
